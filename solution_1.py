4c0ace83e0689be4bf8b6670f74849528a28bb45
A.M. Nolan <11098245+amnolan@users.noreply.github.com>
2023-07-06 12:28:23 -0500

    Update solution_1.py

--- solution_1.py
+++ solution_1.py
@@ -2,6 +2,35 @@
 # "dvdf" because it has forgotten about the previous strings it has seen
 # to get this to work it probably needs to go back a bit and rescan working on a full solution
 
+# Given a string s, find the length of the longest 
+# substring
+#  without repeating characters.
+
+ 
+
+# Example 1:
+
+# Input: s = "abcabcbb"
+# Output: 3
+# Explanation: The answer is "abc", with the length of 3.
+# Example 2:
+
+# Input: s = "bbbbb"
+# Output: 1
+# Explanation: The answer is "b", with the length of 1.
+# Example 3:
+
+# Input: s = "pwwkew"
+# Output: 3
+# Explanation: The answer is "wke", with the length of 3.
+# Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ 
+
+# Constraints:
+
+# 0 <= s.length <= 5 * 104
+# s consists of English letters, digits, symbols and spaces.
+
 class SolutionToMostCases:
     # input string returns int value of longest substring with
     # non-repeating values
